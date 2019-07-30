70dc6f57649b2513e3364ad2d6c5adb14c861ee9
Jacob Krevat <jacobtie@gmail.com>
2019-07-30 18:16:28 -0400

    Updates

---
+++ precache-manifest.958b120c91aa6fa34c0820ae3d55b629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0e51d16fe2b3938996599c6d82a8dd",
+    "url": "/initiative-tracker/index.html"
+  },
+  {
+    "revision": "b6e6d1a4bf9380b42590",
+    "url": "/initiative-tracker/static/css/main.bc9aa30c.chunk.css"
+  },
+  {
+    "revision": "e34d9cd5ed13e473a2ac",
+    "url": "/initiative-tracker/static/js/2.c7aed3e4.chunk.js"
+  },
+  {
+    "revision": "b6e6d1a4bf9380b42590",
+    "url": "/initiative-tracker/static/js/main.0e4787e5.chunk.js"
+  },
+  {
+    "revision": "aa26c79c17f1b6604fa0",
+    "url": "/initiative-tracker/static/js/runtime~main.a0faf316.js"
+  },
+  {
+    "revision": "488b494ec3a87075f0d8036630f76f74",
+    "url": "/initiative-tracker/static/media/bg.488b494e.jpg"
+  }
+]);
